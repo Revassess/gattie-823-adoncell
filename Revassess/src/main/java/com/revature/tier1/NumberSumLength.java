984b3014201c135a40f1fbe84c3cda59e89f586b
Annabella <annabelladoncell@gmail.com>
2020-08-26 15:44:48 -0400

    tier1 attempt1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,27 @@
 package com.revature.tier1;
 
+import java.lang.Math;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		String numString = Long.toString(num);
+		char[] charArray = numString.toCharArray();
+		int[] arr = new int[charArray.length];
+		for (int i = 0; i < charArray.length; i++) {
+			arr[i] = Character.getNumericValue(charArray[i]);
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int raised = (int) Math.pow(arr[i], arr.length);
+			sum += raised;
+		}
+		
+		if (sum == num) {
+			return true;
+		}
+		
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,14 @@
 package com.revature.tier1;
 
 public class User {
+	
+	public String id;
+	public String firstName;
+	public String lastName;
+	public String username;
+	public String password;
+	public String role;
+	
+	
+	
 }
